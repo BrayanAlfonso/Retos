e429d4d00aa71a272ef528f48601ba591ff8a6eb
BrayanAlfonso <brayan.alfonso@soy.sena.edu.co>
2023-03-18 20:05:25 -0500

    Actualizado

--- src/Reto8.java
+++ src/Reto8.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Reto8 {
     public static void main(String[] args) {
-        String nombreAprendiz, auxilio, nuevoAprendiz="", bucle;
+        String nombreAprendiz, auxilio, nuevoAprendiz="", bucle, subsidio;
         int i=0, cont=0, numId, edad;
         Scanner lectura=new Scanner(System.in);
 
@@ -20,10 +20,18 @@ public class Reto8 {
     
                 System.out.println("Ingrese la edad del aprendiz");
                 edad=lectura.nextInt();
+
+                System.out.println("El aprendiz estta registrado a algun subsidio como jovenes en accion?");
+                subsidio=lectura.next();
+                if(subsidio.equals("si")){
+                    System.out.println("Lo sentimos debido a que esta afiliado a un subsidio del estado no puede aplicar al beneficio del auxilio");
+                }else{
+                    System.out.println("Ingrese el auxilio al que se va a registrar");
+                    auxilio = lectura.next();
+                    System.out.println("Felicidades el aprendiz "+nombreAprendiz+" ha sido registrado en el auxilio de "+auxilio );
+                }
     
-                System.out.println("Ingrese el auxilio al que se va a registrar");
-                auxilio = lectura.next();
-                System.out.println("Felicidades el aprendiz "+nombreAprendiz+" ha sido registrado en el auxilio de "+auxilio );
+
                 
             }else{
                 System.out.println("Lo sentimos el aprendiz digitado no ha sido encontrado, a continuacion digite el nombre del aprendiz para registrarlo.");
